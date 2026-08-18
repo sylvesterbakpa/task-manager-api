3bdbff0474346b5e615cf065abddd25deb7bb4f3
Sylvester Bakpa <sylvesterbakpa.gmail.com>
2026-08-18 19:19:40 +0100

    Refactor handlers to enable PostgreSQL CRUD operations to replace in-memory map storage. Make DB connection configurable via env vars (DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME) instead of hardcoded values

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"cmp"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
 	"os"
-	"slices"
 	"strconv"
-	"sync"
+	"strings"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -29,9 +27,7 @@ type ErrorResponse struct {
 	Error string `json:"error"`
 }
 
-var tasks = make(map[int]Task)
-var counter int
-var mu sync.Mutex
+var db *sql.DB
 
 func writeError(w http.ResponseWriter, status int, message string) {
 	w.Header().Set("Content-Type", "application/json")
@@ -41,21 +37,39 @@ func writeError(w http.ResponseWriter, status int, message string) {
 }
 
 func GetTasks(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	defer mu.Unlock()
-	taskList := []Task{}
-	for _, task := range tasks {
+	var taskList []Task
+	taskRows, err := db.Query("SELECT id, title, done FROM tasks ORDER BY id")
+	if err != nil {
+		fmt.Println("Query failed:", err)
+		writeError(w, http.StatusInternalServerError, "Failed to retrieve tasks")
+		return
+	}
+	defer taskRows.Close()
+	for taskRows.Next() {
+		var task Task
+		err := taskRows.Scan(&task.ID, &task.Title, &task.Done)
+		if err != nil {
+			fmt.Println("Scan failed:", err)
+			writeError(w, http.StatusInternalServerError, "Failed to retrieve tasks")
+			return
+		}
 		taskList = append(taskList, task)
 	}
-	slices.SortFunc(taskList, func(a, b Task) int {
-		return cmp.Compare(a.ID, b.ID)
-	})
+
+	err = taskRows.Err()
+	if err != nil {
+		fmt.Println("An error occured:", err)
+		writeError(w, http.StatusInternalServerError, "Failed to retrieve tasks")
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(taskList)
 }
 
 func PostTask(w http.ResponseWriter, r *http.Request) {
 	var taskInput CreateTaskInput
+	var task Task
 	err := json.NewDecoder(r.Body).Decode(&taskInput)
 	if err != nil {
 		writeError(w, http.StatusBadRequest, "Invalid JSON in request body")
@@ -65,60 +79,100 @@ func PostTask(w http.ResponseWriter, r *http.Request) {
 		writeError(w, http.StatusBadRequest, "Request cannot be empty")
 		return
 	}
-	mu.Lock()
-	defer mu.Unlock()
-	counter++
-	task := Task{ID: counter, Title: taskInput.Title, Done: false}
-	tasks[counter] = task
+
+	titleLower := strings.ToLower(taskInput.Title)
+	firstChar := strings.ToUpper(string(titleLower[0]))
+	title := firstChar + titleLower[1:]
+
+	insertValues := `
+	INSERT INTO tasks (title, done) 
+	VALUES ($1, $2)
+	RETURNING id
+	`
+	err = db.QueryRow(insertValues, title, false).Scan(&task.ID)
+	if err != nil {
+		fmt.Println("QueryRow/Scan failed:", err)
+		writeError(w, http.StatusInternalServerError, "Failed to add task")
+		return
+	}
+	
+	task = Task{ID: task.ID, Title: title, Done: false}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(task)
 }
 
 func PatchTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
 	idIntValue, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
-		writeError(w, http.StatusBadRequest, "Enter a valid task ID")
+		writeError(w, http.StatusBadRequest, "Invalid task ID")
 		return
 	}
-	mu.Lock()
-	defer mu.Unlock()
-	task, ok := tasks[idIntValue]
-	if ok {
-		task.Done = true
-		tasks[idIntValue] = task
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(task)
 
-	} else {
+	updateValues := `
+	UPDATE tasks
+	SET done = true
+	WHERE id = $1
+	RETURNING id, title, done
+	`
+	err = db.QueryRow(updateValues, idIntValue).Scan(&task.ID, &task.Title, &task.Done)
+	if err == sql.ErrNoRows {
+		fmt.Println(err)
 		writeError(w, http.StatusNotFound, "Task not found")
 		return
+	} else if err != nil {
+		fmt.Println("QueryRow failed", err)
+		writeError(w, http.StatusInternalServerError, "Failed to update task")
+		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(task)
 }
 
 func DeleteTask(w http.ResponseWriter, r *http.Request) {
 	idIntValue, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
-		writeError(w, http.StatusBadRequest, "Enter a valid task ID")
+		writeError(w, http.StatusBadRequest, "Invalid task ID")
 		return
 	}
-	mu.Lock()
-	defer mu.Unlock()
-	_, ok := tasks[idIntValue]
-	if ok {
-		delete(tasks, idIntValue)
-		w.WriteHeader(http.StatusNoContent)
-	} else {
+
+	deleteValues := `
+	DELETE FROM tasks
+	WHERE id = $1
+	`
+	deleteRow, err := db.Exec(deleteValues, idIntValue)
+	if err != nil {
+		fmt.Println("Exec failed:", err)
+		writeError(w, http.StatusInternalServerError, "Failed to delete task")
+		return
+	}
+
+	rowsAffected, err := deleteRow.RowsAffected()
+	if err != nil {
+		fmt.Println("RowsAffected failed:", err)
+		writeError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
+
+	if rowsAffected == 0 {
 		writeError(w, http.StatusNotFound, "Task not found")
 		return
+	} else {
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
 
 func main() {
-	password := os.Getenv("DB_PASSWORD")
-	urlEncPassword := url.QueryEscape(password)
-	db, err := sql.Open("pgx", "postgres://postgres:" + urlEncPassword + "@localhost:5432/task_manager")
+	var err error
+	urlEncUser := url.QueryEscape(os.Getenv("DB_USER"))
+	urlEncPassword := url.QueryEscape(os.Getenv("DB_PASSWORD"))
+	urlEncHost := url.QueryEscape(os.Getenv("DB_HOST"))
+	urlPort := os.Getenv("DB_PORT")
+	urlName := os.Getenv("DB_NAME")
+	db, err = sql.Open("pgx", "postgres://" + urlEncUser + ":" + urlEncPassword + "@" + urlEncHost + ":" + urlPort + "/" + urlName)
 	if err != nil {
 		fmt.Println("Invalid database configuration")
 		return
